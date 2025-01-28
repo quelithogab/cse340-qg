b12ab374af50427a0bd339ed0b3224218aef6956
quelithogab <quelithogabriel28@gmail.com>
2025-01-28 09:49:02 -0500

    Robust error handling added

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 const inventoryRoute = require("./routes/static")
-
+const utilities = require("./utilities");
 const baseController = require("./controllers/baseController")
 
 /* ***********************
@@ -29,7 +29,7 @@ app.set("layout", "./layouts/layout") // not at views root
 app.use(static)
 // Index route
 app.use("/inv", inventoryRoute)
-app.get("/", baseController.buildHome)
+app.get("/", utilities.handleErrors(baseController.buildHome))
 
 // File Not Found Route - must be last route in list
 app.use(async (req, res, next) => {
@@ -41,12 +41,13 @@ app.use(async (req, res, next) => {
 * Place after all other middleware
 *************************/
 app.use(async (err, req, res, next) => {
-  let nav = await utilities.getNav();
-  console.error(`Error at: "${req.originalUrl}": ${err.message}`);
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  if(err.status == 404){ message = err.message} else {message = 'Oh no! There was a crash. Maybe try a different route?'}
   res.render("errors/error", {
     title: err.status || 'Server Error',
-    message: err.message,
-    nav,
+    message,
+    nav
   })
 })
 
